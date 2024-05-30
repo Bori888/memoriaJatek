0ecff03f031985c909d2c88d6387202ac8d0181c
Bori888 <ilyes.bori8@gmail.com>
2024-05-30 12:06:34 +0200

    Valami nagyon elromlot csak egy képem van

--- Jatek.js
+++ Jatek.js
@@ -2,13 +2,33 @@ import Kartya from "./Kartya.js"
 
 export default class Jatek{
     #KEPLISTA=[]
+    #kivalasztottak=[]
+
     constructor(KEPLISTA){
         this.#KEPLISTA=KEPLISTA
         this.jatekterOsszeAllit()
+        $(window).on("kapcsol",(event)=>{
+            console.log(event.detail)
+            this.#kivalasztottak.push(event.detail)
+            this.ellenoriz()
+        })
     }
     jatekterOsszeAllit(){
         this.#KEPLISTA.forEach((elem)=>{
             new Kartya(elem,$(".jatekter"))
         })
+    }
+    #ellenoriz(esemenynev){
+        if(this.#kivalasztottak.length===2){
+            if (this.#kivalasztottak[0]===this.#kivalasztottak[1]){
+                /**itt kéne viszaforditani az 2 elmet
+                 * és ki kel üriteni a listát
+                 */
+                this.#kivalasztottak.pop()
+                this.#kivalasztottak.pop()
+                
+            }
+        }
+
     }
 }
--- Kartya.js
+++ Kartya.js
@@ -6,23 +6,54 @@ export default class Kartya{
     #kep=""
     #allapot = true;/*tru= láthato a kép false láthato a hátoldal */
     #szuloElem;/*dom elem, hátér elem */
+    #kepElem
     constructor(kep , szuloElem){
         this.#kep=kep;
         this.#szuloElem=szuloElem
         this.#allapot=false
         this.#megjelenit()
+        this.kepElem=this.#szuloElem.children("div:last-child").children("img")
+        console.log(this.#kepElem)
+        this.#kepElem.om("click",()=>{
+            this.setAllapot(true)
+            this.#trigger(kapcsol)
+        })
 
     }
+    #trigger(esemenynev){
+        const e= new CustomEvent(esemenynev,{detail:this.#kep})
+        window.dispatchEvent(e)
+
+    }
+
     #megjelenit(){
         let txt=`
         <div class="card col-sm-3">
-            <img src="${this.#kep}" alt="memoriajáték kiskep">
+            <img src="kepek/hatter.jpg" alt="memoriajáték kiskep">
         </div>
         `
         this.#szuloElem.append(txt)
         
     }
-    felfordit(){
+    setAllapot(ertek){
+        if (ertek==true||ertek== false) {
+            this.#allapot=ertek
+            this.#felfordit()
+            
+        }
+        
+    }
+    #felfordit(){
+        /*Az állapottol fuggöen vagy a h.k. vagy a k. et toldi be a kep src-jbe */
+        /*Akor kell végrehajtodni ha ráé katintunk */
+        if (this.#allapot){
+            this.#kepElem.attr("src",this.#kep)
+        }
+        else{
+            this.#kepElem.attr("src","kepek/hatter.jpg")
+
+        }
+
 
     }
 }
